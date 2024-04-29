efa856a94238752b13686ba7a03006ff06ecbd34
Junu Lee <junu0516@kakao.com>
2024-04-29 17:25:43 +0900

    Time: 18 ms (92.15%), Space: 17.1 MB (5.29%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.swift
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.swift
@@ -1,6 +1,6 @@
 class Solution {
     func removeDuplicates(_ nums: inout [Int]) -> Int {
-        nums = Set(nums).sorted()
+        nums = Set(nums).sorted() // 중복 제거 후 다시 정렬
         return nums.count
     }
 }
