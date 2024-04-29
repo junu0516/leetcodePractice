d3ae36d2db67dfd35c6c192dc4737da3d767a817
Junu Lee <junu0516@kakao.com>
2024-04-29 22:55:41 +0900

    Time: 318 ms (26.19%), Space: 16.6 MB (23.81%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.swift
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.swift
@@ -10,26 +10,12 @@ class Solution {
             - 동일한 문자열 s를 연달아 이어 붙이고, 첫번째와 마지막 문자를 제거
             - 이 때 남은 문자열에서 s와 동일한 부분 문자열이 발견되면 됨?
         */
-
-        let chars = Array(s + s)
-        let middleChars = chars[1..<chars.count-1]
-        let substring = String(middleChars)
+        
+        let extendedString = s + s
+        let startIndex = extendedString.index(after: extendedString.startIndex)
+        let endIndex = extendedString.index(before: extendedString.endIndex)
+        let substring = extendedString[startIndex..<endIndex]        
         
         return substring.contains(s)
-    }
-}
-
-extension String {
-    
-    func contains(_ substring: String) -> Bool {
-        guard let range = self.range(of: substring) else {
-            return false
-        }
-        
-        let lowerBound = range.lowerBound
-        let startIndex = self.startIndex
-        let distance = self.distance(from: startIndex, to: lowerBound)
-        
-        return self.distance(from: startIndex, to: lowerBound) >= 0
     }
 }
