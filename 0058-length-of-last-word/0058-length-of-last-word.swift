b47cbb6f65055fdc477fa422fc91f2b05eace994
Junu Lee <junu0516@kakao.com>
2024-04-30 22:39:06 +0900

    Time: 10 ms (12.99%), Space: 16.8 MB (10.96%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.swift
+++ 0058-length-of-last-word/0058-length-of-last-word.swift
@@ -1,9 +1,12 @@
 class Solution {
     func lengthOfLastWord(_ s: String) -> Int {
+        // 양끝 공백 우선 제거하고 시작
         let s = s.trimmingCharacters(in: .whitespaces)
+        
         let chars = Array(s)
         var maxLength = 0
         
+        // 반복문 돌면서 단어 사이의 공백 고려
         for char in chars {
             if char == " " {
                 maxLength = 0
